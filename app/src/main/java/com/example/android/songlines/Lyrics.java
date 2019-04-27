7f025b2fa384dc0f47a5b302f87a382aa4bd1a28
Ritik Harchani <harchani.ritik@gmail.com>
2019-04-27 06:58:56 +0530

    Color Changes

--- app/src/main/java/com/example/android/songlines/Lyrics.java
+++ app/src/main/java/com/example/android/songlines/Lyrics.java
@@ -7,8 +7,8 @@ import android.widget.TextView;
 
 public class Lyrics extends AppCompatActivity {
 
-    TextView lyrics,K1,K2,K3,K4,K6,K7,V1,V2,V3,V4,V6,V7;
-    Typeface lyricsfonts;
+    TextView title,lyricsTitle,lyrics,K1,K2,K3,K4,K6,K7,V1,V2,V3,V4,V6,V7;//Key-Value Pairs
+    Typeface mycustomfont,anotherfont,newfont;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +16,8 @@ public class Lyrics extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_lyrics);
         lyrics=(TextView)findViewById(R.id.Lyrics);
-        lyrics.setText(MainActivity.trackLyrics);
+        title=(TextView)findViewById(R.id.textView) ;
+        lyricsTitle=(TextView)findViewById(R.id.lyricstextview);
 
         K1=(TextView)findViewById(R.id.k1);
         K2=(TextView)findViewById(R.id.k2);
@@ -32,6 +33,7 @@ public class Lyrics extends AppCompatActivity {
         V6=(TextView)findViewById(R.id.v6);
         V7=(TextView)findViewById(R.id.v7);
 
+        lyrics.setText(MainActivity.trackLyrics);
         V1.setText(MainActivity.trackName);
         V2.setText(MainActivity.artName);
         V3.setText(MainActivity.albumName);
@@ -39,8 +41,27 @@ public class Lyrics extends AppCompatActivity {
         V6.setText(MainActivity.yLink);
         V7.setText(MainActivity.des);
 
-        lyricsfonts=Typeface.createFromAsset(getAssets(),"fonts/cursive.ttf");
-        lyrics.setTypeface(lyricsfonts);
+        //creating font types
+        mycustomfont= Typeface.createFromAsset(getAssets(),"fonts/almedra.ttf");
+        anotherfont=Typeface.createFromAsset(getAssets(),"fonts/alice-regular.ttf");
+        newfont=Typeface.createFromAsset(getAssets(),"fonts/cursive.ttf");
+
+        //assigning fonts
+        lyrics.setTypeface(newfont);
+        title.setTypeface(mycustomfont);
+        lyricsTitle.setTypeface(mycustomfont);
+        K1.setTypeface(anotherfont);
+        K2.setTypeface(anotherfont);
+        K3.setTypeface(anotherfont);
+        K4.setTypeface(anotherfont);
+        K6.setTypeface(anotherfont);
+        K7.setTypeface(anotherfont);
+        V1.setTypeface(newfont);
+        V2.setTypeface(newfont);
+        V3.setTypeface(newfont);
+        V4.setTypeface(newfont);
+        V6.setTypeface(newfont);
+        V7.setTypeface(newfont);
 
         //trackName,artName,albumName,genre,mvd,yLink,des
 
