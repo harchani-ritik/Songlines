29b4b561d1aaf37571c6880266006320afeb6082
Ritik Harchani <harchani.ritik@gmail.com>
2019-04-27 13:06:41 +0530

    Changes

--- app/src/main/java/com/example/android/songlines/MainActivity.java
+++ app/src/main/java/com/example/android/songlines/MainActivity.java
@@ -1,11 +1,8 @@
 package com.example.android.songlines;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Typeface;
 import android.os.AsyncTask;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
